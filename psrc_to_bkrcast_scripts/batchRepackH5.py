34e7c9d6906fa8e81a3db8a454a6070709fc110f
bstabler <benstabler@yahoo.com>
2016-10-20 14:55:57 -0700

    batch repack h5 files

---
+++ psrc_to_bkrcast_scripts/batchRepackH5.py
@@ -0,0 +1,24 @@
+
+# Batch repack HDF5 files
+# python.exe batchRepackH5.py
+# Ben.Stabler@rsginc.com, 10/20/16
+
+import os
+import fnmatch
+
+ext = "*.H5"
+h5repack = '"C:\\Program Files\\HDF_Group\\HDF5\\1.8.17\\bin\\h5repack.exe"'
+
+matches = []
+
+#find files
+for root, dirnames, filenames in os.walk(os.getcwd()):
+    for filename in fnmatch.filter(filenames, ext):
+        matches.append(os.path.join(root, filename))
+
+#repack files
+for item in matches:
+    itemOut = item.replace(".h5","_repack.h5")
+    cmdLine = h5repack + " %s %s" % (item, itemOut)
+    os.system(cmdLine)
+    print cmdLine
