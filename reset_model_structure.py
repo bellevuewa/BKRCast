ee551c26c8f848c286882c4dcf1e4fbc3019df96
Hu <hdong@bellevuewa.gov>
2019-12-17 10:15:17 -0800

    Create a tool to reset the model structure, by removing outputs and other files that are derived from input files.

---
+++ reset_model_structure.py
@@ -0,0 +1,28 @@
+import os
+from input_configuration import *
+import shutil
+
+print 'resetting....'
+
+def removeDir(path):
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+        print path + ' is removed.'
+
+def removeFile(path):
+    if os.path.isfile(path):
+        os.remove(path)
+        print path + ' is removed'
+
+
+dirs_for_removal = ['inputs/4k', 'inputs/supplemental', 'outputs', 'inputs/bikes', 'inputs/accessibility', 'inputs/trucks', 'inputs/Fares', 'inputs/IntraZonals', 'inputs/vdfs', 'inputs/tolls',
+                    'inputs/extra_attributes', 'inputs/observed', 'inputs/networks', 'projects', 'banks', 'daysim', 'working']
+files_for_removal = ['inputs/parking_gz.csv', 'inputs/lu_type.csv', 'inputs/p_r_nodes.csv', households_persons_file, 'inputs/6to9.h5', 'inputs/9to1530.h5', 'inputs/1530to1830.h5', 'inputs/1830to6.h5']
+
+for file in files_for_removal:
+    removeFile(os.path.join(project_folder, file))
+
+for dir in dirs_for_removal:
+    removeDir(os.path.join(project_folder,dir))
+
+print 'Reset.'
