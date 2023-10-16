e757738be804370b4eec0fe9762ddbf25cb48c30
Hu Dong <HDong@bellevuewa.gov>
2023-10-16 16:49:25 -0700

    put back parcel_summary.py because it was deleted by a mistake.

---
+++ scripts/summarize/standard/parcel_summary.py
@@ -0,0 +1,48 @@
+# Load buffered parcel data and summarize by Regional Growth Center
+import pandas as pd
+import numpy as np
+import os, sys
+sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(),"scripts"))
+
+from input_configuration import *# Import as a module to access inputs as a dictionary
+from accessibility.accessibility_configuration import *
+
+# 10/25/2021
+# modified to be compatible with python 3
+
+print("running parcel summary")
+
+parcel_urbcen_map = 'parcels_in_urbcens.csv'    # lookup for parcel to RGC
+file_out = 'parcel_summary.xlsx'    # summary output file name
+
+# Load files in pandas
+main_dir = os.path.abspath('')
+try:
+    parcels = pd.read_csv(os.path.join(project_folder, output_parcels), sep=' ')
+except:
+    print("Missing 'buffered_parcels.dat'")
+
+try:
+    map = pd.read_csv(main_dir + "/scripts/summarize/inputs/" + parcel_urbcen_map)
+except:
+    print("Missing 'parcels_in_urbcens.csv'")
+
+
+# Join the urban center location to the parcels file
+parcels = pd.merge(parcels, map, left_on='parcelid', right_on='hhparcel')
+print("Loading parcel data to summarize...")
+
+# Summarize parcel fields by urban center
+mean_by_urbcen = pd.DataFrame(parcels.groupby('NAME').mean())
+min_by_urbcen = pd.DataFrame(parcels.groupby('NAME').min())
+max_by_urbcen = pd.DataFrame(parcels.groupby('NAME').max())
+std_by_urbcen = pd.DataFrame(parcels.groupby('NAME').std())
+
+# Write results to separate worksheets in an Excel file
+excel_writer = pd.ExcelWriter(main_dir + '/outputs/summary' + file_out)
+
+mean_by_urbcen.to_excel(excel_writer=excel_writer, sheet_name='Mean')
+min_by_urbcen.to_excel(excel_writer=excel_writer, sheet_name='Min')
+max_by_urbcen.to_excel(excel_writer=excel_writer, sheet_name='Max')
+std_by_urbcen.to_excel(excel_writer=excel_writer, sheet_name='Std Dev')
