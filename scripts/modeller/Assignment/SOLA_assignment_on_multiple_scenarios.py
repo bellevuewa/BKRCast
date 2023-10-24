095b66cef31b7767a118e9018978946936668fe6
Hu Dong <HDong@bellevuewa.gov>
2023-10-24 15:13:21 -0700

    fix #110

--- scripts/modeller/Assignment/SOLA_assignment_on_multiple_scenarios.py
+++ scripts/modeller/Assignment/SOLA_assignment_on_multiple_scenarios.py
@@ -7,23 +7,28 @@ import datetime
 import os
 import json as _json
 
-class BKRCastExportAllAttributes(_modeller.Tool()):
+class BKRCastSOLAAssignments(_modeller.Tool()):
     '''
     1.1: upgrade to python 3.7, compatible with EMME 4.5.1
+    1.2  now the assignment setting is consistent with SkimsAndPaths.py.    
     '''
-    version = "1.1" # this is the version
+    version = "1.2" # this is the version
     default_path = ""
     tool_run_message = ""
     scenarios_list = _modeller.Attribute(list)
-    sola_specs_box = _modeller.Attribute(str)
+    sola_spec_file = _modeller.Attribute(str)
+    user_class_file = _modeller.Attribute(str)        
 
     def page(self):
         pb = _modeller.ToolPageBuilder(self, title="BKRCast Network Interface",
                      description="SOLA Assignment on Multiple Scenarios",
                      branding_text="Modeling and Analysis Group -- City of Bellevue Transportation")
         pb.add_select_scenario("scenarios_list", title = "Select scenarios")
-        pb.add_sola_traffic_assignment_spec("sola_specs_box", title = "Select specs")
-
+        pb.add_select_file("sola_spec_file", 'file', title = "Select SOLA assignment specs")
+        pb.add_select_file('user_class_file', 'file', title = 'Select user class file')    
+        self.sola_spec_file = '../../../inputs/skim_params/SOLA_assignment.json' 
+        self.user_class_file = '../../../inputs/skim_params/user_classes.json'            
+        print(os.getcwd())
         if self.tool_run_message != "":
             pb.tool_run_status(self.tool_run_msg_status)
 
@@ -50,21 +55,42 @@ class BKRCastExportAllAttributes(_modeller.Tool()):
     def run(self):
         self.tool_run_message = ""
         try:
-            self.__call__()
-            run_message = "All attributes exported"
+            self.__call__(self.scenarios_list, self.sola_spec_file, self.user_class_file)
+            run_message = "All assignments are done."
             self.tool_run_message += _modeller.PageBuilder.format_info(run_message)
         except Exception as e:
             self.tool_run_message += _modeller.PageBuilder.format_exception(exception = e, chain = False)
+            
+    def create_spec(self, sola_spec_file, user_class_file):
+        # build SOLA based assignment specs based on template, match the code in SkimsAndPaths.py   
+        sola_specs = _json.load(open(sola_spec_file))
+        user_class = _json.load(open(user_class_file))   
 
-    @_modeller.logbook_trace(name="BKRCast Export All Attributes", save_arguments=True)
-    def __call__(self):
+        sola_specs["stopping_criteria"]["max_iterations"]= 5
+        sola_specs["stopping_criteria"]["best_relative_gap"]= 0.001 
+        sola_specs["stopping_criteria"]["relative_gap"]= 0.001
+
+        for x in range (0, len(sola_specs["classes"])):
+            vot = ((1/float(user_class["Highway"][x]["Value of Time"]))*60)    # reciprocol of VOT converted to cents/minute.
+            sola_specs["classes"][x]["generalized_cost"]["perception_factor"] = vot
+            sola_specs["classes"][x]["generalized_cost"]["link_costs"] = user_class["Highway"][x]["Toll"]
+            sola_specs["classes"][x]["demand"] = "mf"+ user_class["Highway"][x]["Name"]
+            sola_specs["classes"][x]["mode"] = user_class["Highway"][x]["Mode"]
+
+        return sola_specs
+                
+    @_modeller.logbook_trace(name="BKRCast SOLA Assignments", save_arguments=True)
+    def __call__(self, scenarios_list, sola_spec_file, user_class_file):
 
         current_scen = self.current_scenario
-        specs = _json.loads(self.sola_specs_box)
-        with _modeller.logbook_trace(name = "SOLA Assignment on Multiple Scenarios", value = ""):
+        sola_specs = self.create_spec(sola_spec_file, user_class_file)
+        with _modeller.logbook_trace(name = "SOLA Assignment on Multiple Scenarios", save_arguments = True, value = ""):
             NAMESPACE = 'inro.emme.traffic_assignment.sola_traffic_assignment'
             sola_assign = _modeller.Modeller().tool(NAMESPACE)
-            for scen in self.scenarios_list:
+            assign_extras = _modeller.Modeller().tool("inro.emme.traffic_assignment.set_extra_function_parameters")
+            assign_extras(el1 = "@rdly", el2 = "@trnv3")
+            
+            for scen in scenarios_list:
                 _modeller.Modeller().desktop.data_explorer().replace_primary_scenario(scen)
-                sola_assign(specs)
+                sola_assign(sola_specs)
  
