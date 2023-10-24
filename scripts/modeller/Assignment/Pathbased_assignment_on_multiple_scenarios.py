9ee3e37bea768b2cb20c015e0a4078718503c365
Hu Dong <HDong@bellevuewa.gov>
2023-10-24 15:13:40 -0700

    fix #109

--- scripts/modeller/Assignment/Pathbased_assignment_on_multiple_scenarios.py
+++ scripts/modeller/Assignment/Pathbased_assignment_on_multiple_scenarios.py
@@ -7,23 +7,27 @@ import datetime
 import os
 import json as _json
 
-class BKRCastExportAllAttributes(_modeller.Tool()):
+class BKRCastPathBasedAssignments(_modeller.Tool()):
     '''
     1.1: upgrade to python 3.7, compatible with EMME 4.5.1
+    1.2  now the assignment setting is consistent with SkimsAndPaths.py.    
     '''
-    version = "1.1" # this is the version
+    version = "1.2" # this is the version
     default_path = ""
     tool_run_message = ""
     scenarios_list = _modeller.Attribute(list)
-    path_specs_box = _modeller.Attribute(str)
-
+    path_based_spec_file = _modeller.Attribute(str)
+    user_class_file = _modeller.Attribute(str)        
+    
     def page(self):
         pb = _modeller.ToolPageBuilder(self, title="BKRCast Network Interface",
                      description="Path based Assignment on Multiple Scenarios",
                      branding_text="Modeling and Analysis Group -- City of Bellevue Transportation")
         pb.add_select_scenario("scenarios_list", title = "Select scenarios")
-        pb.add_path_based_traffic_assignment_spec("path_specs_box", title = "Select specs")
-
+        pb.add_select_file("path_based_spec_file", 'file', title = "Select path based assignment specs")
+        pb.add_select_file('user_class_file', 'file', title = 'Select user class file')    
+        self.path_based_spec_file = '../../../inputs/skim_params/general_path_based_assignment.json' 
+        self.user_class_file = '../../../inputs/skim_params/user_classes.json'            
         if self.tool_run_message != "":
             pb.tool_run_status(self.tool_run_msg_status)
 
@@ -50,23 +54,42 @@ class BKRCastExportAllAttributes(_modeller.Tool()):
     def run(self):
         self.tool_run_message = ""
         try:
-            self.__call__()
-            run_message = "All attributes exported"
+            self.__call__(self.scenarios_list, self.path_based_spec_file, self.user_class_file)
+            run_message = "All assignments are done"
             self.tool_run_message += _modeller.PageBuilder.format_info(run_message)
         except Exception as e:
             self.tool_run_message += _modeller.PageBuilder.format_exception(exception = e, chain = False)
 
-    @_modeller.logbook_trace(name="BKRCast Export All Attributes", save_arguments=True)
-    def __call__(self):
+    def create_spec(self, path_based_spec_file, user_class_file):
+        # build PAth based assignment specs based on template, match the code in SkimsAndPaths.py   
+        pathbased_specs = _json.load(open(path_based_spec_file))
+        user_class = _json.load(open(user_class_file))   
+
+        pathbased_specs["stopping_criteria"]["max_iterations"]= 100
+        pathbased_specs["stopping_criteria"]["best_relative_gap"]= 0.001 
+        pathbased_specs["stopping_criteria"]["relative_gap"]= 0.001
+
+        for x in range (0, len(pathbased_specs["classes"])):
+            vot = ((1/float(user_class["Highway"][x]["Value of Time"]))*60)    # reciprocol of VOT converted to cents/minute.
+            pathbased_specs["classes"][x]["generalized_cost"]["perception_factor"] = vot
+            pathbased_specs["classes"][x]["generalized_cost"]["link_costs"] = user_class["Highway"][x]["Toll"]
+            pathbased_specs["classes"][x]["demand"] = "mf"+ user_class["Highway"][x]["Name"]
+            pathbased_specs["classes"][x]["mode"] = user_class["Highway"][x]["Mode"]
+
+        return pathbased_specs            
+            
+    @_modeller.logbook_trace(name="BKRCast Path Based Assignment on Multiple Scenarios", save_arguments=True)
+    def __call__(self, scenarios_list, path_based_spec_file, user_class_file):
 
         current_scen = self.current_scenario
-
+        pathbased_specs = self.create_spec(path_based_spec_file, user_class_file)
         #export extra attributes
-        with _modeller.logbook_trace(name = "Path Based Assignment on Multiple Scenarios", value = ""):
+        with _modeller.logbook_trace(name = "Path Based Assignment on Multiple Scenarios", save_arguments=True, value = ""):
             NAMESPACE = 'inro.emme.traffic_assignment.path_based_traffic_assignment'
             path_assign = _modeller.Modeller().tool(NAMESPACE)
-            specs = _json.loads(self.path_specs_box)
-            for scen in self.scenarios_list:
+            assign_extras = _modeller.Modeller().tool("inro.emme.traffic_assignment.set_extra_function_parameters")
+            assign_extras(el1 = "@rdly", el2 = "@trnv3")
+            for scen in scenarios_list:
                 _modeller.Modeller().desktop.data_explorer().replace_primary_scenario(scen)
-                path_assign(specs)
+                path_assign(pathbased_specs)
  
