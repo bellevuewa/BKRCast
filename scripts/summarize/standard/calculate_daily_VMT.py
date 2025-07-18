9493e7dd62a1ebec78701eb9c0979bf2a0b5815d
hu dong <hdong@bellevuewa.gov>
2025-07-18 14:11:12 -0700

    Refactor calculate_daily_VMT.py for adding controid connectors to VMT calculation as an option.

--- scripts/summarize/standard/calculate_daily_VMT.py
+++ scripts/summarize/standard/calculate_daily_VMT.py
@@ -17,6 +17,10 @@ from data_wrangling import *
 # calculate VMT/VHT/VHD by link speed in 5MPH interval.
 
 # file will be exported to default output folder.
+
+# 7/17/2025
+# added option to include centroid connectors in the calculation
+
 outputfilename = 'system_metrics.txt'
 
 def calculate_system_metrics(links_df, tod, groupby):
@@ -49,21 +53,25 @@ def help():
     print(' python calculate_daily_VMT.py -h -t extra_link_attribute_tag')
     print('    -h: help')
     print('    -t: customized extra link attribute for link tagging')
+    print('    -c: include centroid connectors in the calculation, default is False')
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 't:')
+        opts, args = getopt.getopt(sys.argv[1:], 'hct:')
     except getopt.GetoptError:
         help()
         sys.exit(2)
     
     attr = '@bkrlink'
+    centroid_connectors_included = False
     for opt, arg in opts:
         if opt == '-h':
             help()
             sys.exit(0)
         elif opt == '-t':
             attr = arg
+        elif opt == '-c':
+            centroid_connectors_included = True
     
     print(network_summary_project)
     my_project = EmmeProject(network_summary_project)
@@ -82,10 +90,12 @@ def main():
 
         links_df = my_project.emme_links_to_df()
         print(f'calculating vmt, vht, and vhd in {value}')
-        auto_non_connectors_df = links_df.loc[(links_df['isAuto'] == True) & (links_df['isConnector'] == False)].copy()
-        ret = calculate_system_metrics(auto_non_connectors_df, value, attr)
+        if not centroid_connectors_included: # exclude centroid connectors
+            links_df = links_df.loc[links_df['isConnector'] == False].copy()
+        auto_network_df = links_df.loc[(links_df['isAuto'] == True)].copy()
+        ret = calculate_system_metrics(auto_network_df, value, attr)
         metrics = pd.merge(metrics, ret, how = 'outer', left_index = True, right_index = True)
-        ret = calculate_for_GHG(auto_non_connectors_df, value, attr)
+        ret = calculate_for_GHG(auto_network_df, value, attr)
         GHG_metrics_list.append(ret)
 
     metrics.fillna(0)
@@ -130,7 +140,10 @@ def main():
 
         f.write('%s\n\n' % ghg_metric.to_string())
         f.write('Notes\n')
-        f.write('1. Auto mode only. Centroid connectors are not included.\n')
+        if centroid_connectors_included:
+            f.write('1. Auto mode only. Centroid connectors are included.\n')
+        else:
+            f.write('1. Auto mode only. Centroid connectors are not included.\n')
         f.write(f'2. {attr}: {groupby_description}')
 
 
