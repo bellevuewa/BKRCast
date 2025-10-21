6a6b021a0e05d7b48199b757916b1433f8771707
oangah <oangah@bellevuewa.gov>
2025-10-21 11:22:23 -0700

    [update] #166. Moved the output files to output folder and cleaned up the scripts folder

--- run_bkrcast_validation.py
+++ run_bkrcast_validation.py
@@ -1,8 +1,9 @@
 import os, sys, time
 import nbformat
+import yaml
+import shutil
 from nbconvert.preprocessors import ExecutePreprocessor
 from input_configuration import project_folder, run_bkrcast_summary
-import yaml
 
 
 def run_ipynb(sheet_name, nb_path):
@@ -26,12 +27,23 @@ def create_quarto_notebook(notebook_name, summary_list, scripts_dir, output_fold
 
     for sheet_name in summary_list:
         run_ipynb(sheet_name, scripts_dir)
+    print(f'Rendering {notebook_name} tables...')
 
     # render quarto book
     text = "quarto render " + scripts_dir
     os.system(text)
-    print(notebook_name + " created")
-    print(f'To open the BKRCast Summary, go to {scripts_dir}\{output_folder} and double click "index.html"')
+
+    # remove existing data first
+    output_dir = os.path.join(project_folder, 'outputs', 'summary', output_folder)
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    # move these files to output folder
+    qproduct_folder = os.path.join(project_folder, scripts_dir, output_folder)
+    if not os.path.exists(qproduct_folder):
+        os.makedirs(qproduct_folder)
+    shutil.move(qproduct_folder, output_dir)
+
+    print(f'To open the BKRCast Summary, go to {output_dir} and double click on "index.html"')
 
 
 def main():
@@ -47,7 +59,7 @@ def main():
         # locate chapters list in common places
         chapters = [chapter.split('.')[0] for chapter in qcfg['book']['chapters'] if '.qmd' not in chapter]
 
-        create_quarto_notebook(notebook_name = "bkrcast_summary-notebook",
+        create_quarto_notebook(notebook_name = "bkrcast_validation",
                                summary_list = chapters,
                                scripts_dir = r"scripts\summarize\calibration\notebooks", 
                                output_folder = qcfg['project']['output-dir']) 
