793611c3eabe46081f4bff1997074afe59c2c7e1
hu dong <hdong@bellevuewa.gov>
2025-11-14 14:58:30 -0800

    update bike time weight and signal delay (for bike only) calculation in network importer

--- scripts/network/network_importer.py
+++ scripts/network/network_importer.py
@@ -242,6 +242,8 @@ def run_importer(project_name, delete_all_scenarios = True):
         if create_no_toll_network == True:
             change_mode_for_no_toll_traffic(my_project)
 
+        # calculate signal delay for bike and store in @bsigdelay (link extra attribute)
+        # @bsigdelay is created if it does not exist or overwritten otherwise.
         my_project.signal_delay_for_bike()
         my_project.bank.dispose()
     my_project.desktop.close()
